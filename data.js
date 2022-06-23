5e8bed39532adc78d85c173a755c3079152fa150
Leia Harlow <leia.harlow@gmail.com>
2022-06-23 16:50:48 -0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,57 @@
+const data = [
+  {
+    product: 'Doritos',
+    description: "The Doritos brand is all about boldness. If you're up to the challenge, grab a bag of Doritos tortilla chips and get ready to make some memories you won't soon forget. It's a bold experience in snacking and beyond.",
+    counter: 5,
+    image: "https://www.doritos.com/sites/doritos.com/files/2018-08/new-nacho-cheese.png",
+    stores: ['Walmart', 'Target', 'Vons']
+  },
+
+  {
+    product: 'Redvines Black Licorice',
+    description: "The best black licorice pairings are salty snacks like chips or popcorn, or other sweet treats like ice cream. Manufactured by the American Licorice® Company, RED VINES candy has held a sweet spot in the heart of candy lovers for five generations.",
+    counter: 0,
+    image: "https://www.kroger.com/product/images/large/front/0004136400283",
+    stores: ['Walmart', 'Vons']
+  },
+
+  {
+    product: 'Pocky Heartful',
+    description: "This large box has heart-shaped biscuit sticks that are dipped in cherry chocolate! It has two separate packs that you can share with your friends or with your special someone.",
+    counter: 11,
+    image: "https://www.japancandystore.com/media/catalog/product/cache/11/image/800x/040ec09b1e35df139433887a97daa66f/2/0/20220112_122.jpg",
+    stores: ['Daiso', 'Vons', 'Mitsuwa']
+  },
+
+  {
+    product: 'Pringles Original',
+    description: "Stack, snack, and savor at home and on the go; Pop open a can for sports games, TV watching, party time or any time",
+    counter: 5,
+    image: "https://www.caseys.com/medias/sys_master/images/ha1/h1c/8851178291230/3800013841_base-400x400/3800013841-base-400x400.png",
+    stores: ['Target', 'Vons', 'Safeway']
+  },
+
+  {
+    product: 'Twizzlers',
+    description: "A perfect movie marathon treat and classic everyday, strawberry-flavored chewy snack that provides long-lasting goodness",
+    counter: 0,
+    image: "https://i5.walmartimages.com/asr/5abe6c05-5d84-41a4-af9b-9a5ba597e9b7.bed84975faabe3380c75dd5b5e9b8c0b.png",
+    stores: ['Walmart', 'Vons', 'Safeway']
+  },
+
+  {
+    product: 'Sweet Tart Ropes (Watermelon Berry)',
+    description: "SweeTARTS Rope Collision takes the classic sweet and tart flavor fusion a step further, creating bendable fun that's fruity licorice and SweeTARTS all in one. 2 Flavors in 1!",
+    counter: 21,
+    image: "https://d2aam04nmhpdf8.cloudfront.net/images/images/000/058/420/xlarge/079200049836-1.png?1619472126",
+    stores: ['Target', 'Vons', 'Safeway']
+  },
+
+  {
+    product: 'Brisk Lemon Iced Tea',
+    description: "https://www.drinkbrisk.com/wp-content/uploads/2020/03/Flavor-Bloom_Template_Lemon.png",
+    counter: 18,
+    image: "",
+    stores: ['7-11', 'Vons', 'Safeway']
+  }
+]
